d03144abf62ff27fed34db0636e89133073d2bb3
sandhyabuilds <sandhyavishwakarma509@gmail.com>
2026-07-07 21:05:29 +0530

    comparison of datatypes

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0); 
+console.log(null >= 0);
+
+// this type of comparison make you confuse
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+//===
+
+console.log("2" === 2);
+
+
+
+
+
+
