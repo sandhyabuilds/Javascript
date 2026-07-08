e91658a74c3e3d623f0882ade80423825974cde6
sandhyabuilds <sandhyavishwakarma509@gmail.com>
2026-07-08 21:29:43 +0530

    Datatypes summary

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,49 @@
+// ********** DATATYPES ************
+
+// 1 => Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100     // return type = number
+const scoreValue = 100.3   // return type = number
+
+const name = "Sandhya"
+
+const isLoggedIn = false   // return type = boolean
+const outsideTemp = null   // return type = object
+let userEmail;             // return type = undefined
+
+const id = Symbol('123')   // return type = symbol
+const anotherId = Symbol('123')   // return type = symbol
+
+console.log(id === anotherId);
+
+// const bigNumber = 3453622798975365646n  // Return type = bigint
+
+// 2 => Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj =
+{
+    name: "Sandhya",
+    age: 22,
+}
+
+// function declaration
+
+function sayAbhay() {
+    console.log("Abhay")
+
+}                             // return type = object function 
+
+sayAbhay() // function callx
+
+const myFunction = function () {
+    console.log("Hello world");
+}
+
+myFunction()
+
+console.log(typeof name);
