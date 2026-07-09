f9db97589a9ff75578376757b9ae0491a01a2c1a
sandhyabuilds <sandhyavishwakarma509@gmail.com>
2026-07-09 19:31:54 +0530

    All about strings

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,25 @@
+const name = "sandhya"
+const repoCount = 4
+
+// console.log(name + repoCount + "value");    //outdated way 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('sandhyavis')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
+
+
+
+
+
+
+
