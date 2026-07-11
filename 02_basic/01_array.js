64a0a60365033104ca167557d774b205b73fd13c
sandhyabuilds <sandhyavishwakarma509@gmail.com>
2026-07-11 11:55:52 +0530

    Array basic

---
+++ 02_basic/01_array.js
@@ -0,0 +1,46 @@
+// array
+
+const myArray = [0, 1, 2, 3, 4, 5]
+const myHeros = ["myFATHER", "myBROTHER", "Spiderman"]
+
+
+// console.log(myArray[0]);
+
+// Array methods
+
+// myArray.push(5)
+// myArray.pop()
+
+// console.log(myArray);
+// myArray.shift()
+
+// console.log(myArray.includes(4));
+// console.log(myArray.indexOf(4));
+
+const newArr = myArray.join()
+
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// console.log(myArray);
+
+// +++++++ Slice and Spice ++++++++++
+
+console.log("A ", myArray);
+
+const myn1 = myArray.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArray);
+
+
+const myn2 = myArray.splice(1, 3)
+console.log(myn2);
+console.log("C ", myArray);
+
+
+
+
+
+
+
