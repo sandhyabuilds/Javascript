3a44ea2438d49335d70f5d699f7936066779ac61
sandhyabuilds <sandhyavishwakarma509@gmail.com>
2026-07-12 21:48:13 +0530

    JSON

---
+++ 02_basic/04_Objects.js
@@ -0,0 +1,89 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc",
+    tinderUser.name = "Sandy",
+    tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "sandhya@google.com",
+    fullname: {
+        username: {
+            firstname: "Sandhya",
+            lastname: "Vishwakarma",
+        }
+    }
+}
+
+// console.log(regularUser.fullname.username.firstname);
+
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "a", 4: "b" }
+const obj4 = { 5: "a", 6: "b" }
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = { ...obj1, ...obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)               // {} => target  obj1 obj2... => source
+const obj3 = { ...obj1, ...obj2 }     // spread operetor
+
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "sandhya@gmail.com"
+    },
+    {
+        id: 1,
+        email: "sandhya@gmail.com"
+    },
+    {
+        id: 1,
+        email: "sandhya@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "Javascript",
+    price: 999,
+    courseInstructor: "Hitesh"
+}
+
+// course.courseInstructor 
+
+const { courseInstructor: Instructor } = course
+// console.log(courseInstructor);
+console.log(Instructor);
+
+// {
+//     "coursename" : "javascript",
+//     "price ": "frameElement",
+//    " courseInstructor": "hiteshchoudhary",
+// }
+
+
+[
+    {},
+    {},
+    {},
+]
+
+
+
+
+
+
+
+
